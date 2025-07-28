ca16fc1a666091a839b3a5d536b57dbf95bddc97
Harry Moore <harrisoncmoore@gmail.com>
2025-07-29 09:30:13 +1200

    Update NCEA Credit Tracker V.1.py
    Added llogin systrem

--- NCEA Credit Tracker V.1.py
+++ NCEA Credit Tracker V.1.py
@@ -13,36 +13,30 @@ def show_frame(frame):
 container = Frame(root)
 container.pack(fill="both", expand=True)
 
-#Frame 1: Main Menu
-frame_main = Frame(container)
-frame_main.place(relwidth=1, relheight=1)
+#Frame 1: Login Menu
+login_menu = Frame(container)
+login_menu.place(relwidth=1, relheight=1)
 
-title_label = Label(frame_main, text="NCEA Credit Tracker", font=("Arial", 21, "bold"))
-
+title_label = Label(login_menu, text="NCEA Credit Tracker", font=("Arial", 21, "bold"))
 title_label.pack(pady=10)
 
-#Button row 
-button_frame = Frame(frame_main)
+username = Entry(login_menu, text="Username: ", font=("Arial", 21))
+username.grid(row=1, column=0)
+
+#Button rows
+button_frame = Frame(login_menu)
 button_frame.pack()
 
-btn_cent = Button(button_frame, text="to Centigrade", bg="yellow", font=style, command=lambda: show_frame(frame_cent))
-btn_cent.pack(side=LEFT, padx=10)
+Login = Button(button_frame, text="Enter", borderwidth=2.5, relief="solid", font=style, command=lambda: show_frame(Main_menu))
+Login.pack(padx=10)
 
-btn_fahr = Button(button_frame, text="to Fahrenheit", bg="red", font=style, command=lambda: show_frame(frame_fahr))
-btn_fahr.pack(side=LEFT, padx=10)
+#Frame 2: Credit tracker menu
+Main_menu = Frame(container)
+Main_menu.place(relwidth=1, relheight=1)
 
-#Frame 2: To Centigrade
-frame_cent = Frame(container)
-frame_cent.place(relwidth=1, relheight=1)
-
-Button(frame_cent, text="Back", command=lambda: show_frame(frame_main)).pack()
-
-#Frame 3: To Fahrenheit
-frame_fahr = Frame(container)
-frame_fahr.place(relwidth=1, relheight=1)
-Button(frame_fahr, text="Back", command=lambda: show_frame(frame_main)).pack()
+Button(Main_menu, text="Back", borderwidth=2, relief="solid", command=lambda: show_frame(login_menu)).pack()
 
 #Show main menu initially
-show_frame(frame_main)
+show_frame(login_menu)
 
 root.mainloop()
