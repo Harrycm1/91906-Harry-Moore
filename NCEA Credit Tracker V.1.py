cfe025a83d9aab152994c53024812010da9c8e84
Harry Moore <harrisoncmoore@gmail.com>
2025-07-30 13:11:59 +1200

    Update NCEA Credit Tracker V.1.py
    Fixed username error

--- NCEA Credit Tracker V.1.py
+++ NCEA Credit Tracker V.1.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 root = Tk()
 root.title("NCEA Credit Tracker")
 root.geometry("500x400")
@@ -9,39 +10,35 @@ style = "Arial 15"
 def show_frame(frame):
     frame.tkraise()
 
-#Main container frame
+# Main container frame
 container = Frame(root)
 container.pack(fill="both", expand=True)
 
-#Frame 1: Login Menu
+#Login menu frame
 login_menu = Frame(container)
-login_menu.grid(relwidth=1, relheight=1)
+login_menu.place(relwidth=1, relheight=1)
 
 title_label = Label(login_menu, text="NCEA Credit Tracker", font=("Arial", 21, "bold"))
-title_label.grid(row=0, column=0)
+title_label.pack(pady=(30, 10))
 
 #Username part
-username_label = Label(root, text="username:")
-username_label.grid(row=1, column=0)
+username_label = Label(login_menu, text="Username:")
+username_label.pack(pady=(10, 2))
+username_entry = Entry(login_menu)
+username_entry.pack(pady=(0, 10))
 
-username_entry = Entry(root)
-username_entry.grid(row=1, column=1)
+#Enter Button
+Login = Button(login_menu, text="Enter", borderwidth=2.5, relief="solid", font=style, command=lambda: show_frame(main_menu))
+Login.pack(pady=20)
 
+#Main menu frame
+main_menu = Frame(container)
+main_menu.place(relwidth=1, relheight=1)
 
-#Button rows
-button_frame = Frame(login_menu)
-button_frame.pack()
+back_button = Button(main_menu, text="Back", borderwidth=2, relief="solid", font=style, command=lambda: show_frame(login_menu))
+back_button.pack(pady=20)
 
-Login = Button(button_frame, text="Enter", borderwidth=2.5, relief="solid", font=style, command=lambda: show_frame(Main_menu))
-Login.pack(padx=10)
-
-#Frame 2: Credit tracker menu
-Main_menu = Frame(container)
-Main_menu.place(relwidth=1, relheight=1)
-
-Button(Main_menu, text="Back", borderwidth=2, relief="solid", command=lambda: show_frame(login_menu)).pack()
-
-#Show main menu initially
+#show login menu first
 show_frame(login_menu)
 
 root.mainloop()
