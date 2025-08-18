6ebea6842b9e1653fc53015101718e4f8b16cdb9
Harry Moore <harrisoncmoore@gmail.com>
2025-08-19 09:37:07 +1200

    I have started adding the entry boxs at the bottom of the 2nd window. But now everything doesn't fit in the window so I am tring to figure out what is going on.

--- NCEA Credit Tracker V.3.py
+++ NCEA Credit Tracker V.3.py
@@ -54,7 +54,7 @@ def check_login():
         messagebox.showerror("Login Failed", "Incorrect username or password.")
 
 #Logiin button
-Login = Button(login_menu, text="Enter", borderwidth=2, relief="solid", font=style, command=check_login)
+Login = Button(login_menu, text="Login", borderwidth=2, relief="solid", font=style, command=check_login)
 Login.pack(pady=20)
 
 #Main menu frame
@@ -83,21 +83,62 @@ grid_frame = Frame(main_menu)
 grid_frame.grid(row=1, column=1, columnspan=2, padx=20, pady=20)
 
 #2x2 gridding
-NA_label = Label(grid_frame, text="NA Credits")
+NA_label = Label(grid_frame, text="NA Credits", font=('Arial', 9, 'underline'))
 NA_label.grid(row=0, column=0, padx=10, pady=10)
 
-A_label = Label(grid_frame, text="A Credits")
+NA_amount = Label(grid_frame, text="0")
+NA_amount.grid(row=1, column=0, padx=5, pady=5)
+
+A_label = Label(grid_frame, text="A Credits", font=('Arial', 9, 'underline'))
 A_label.grid(row=0, column=1, padx=10, pady=10)
 
-M_label = Label(grid_frame, text="M Credits")
-M_label.grid(row=1, column=0, padx=10, pady=10)
+A_amount = Label(grid_frame, text="0")
+A_amount.grid(row=1, column=1, padx=5, pady=5)
 
-E_label = Label(grid_frame, text="E Credits")
-E_label.grid(row=1, column=1, padx=10, pady=10)
+M_label = Label(grid_frame, text="M Credits", font=('Arial', 9, 'underline'))
+M_label.grid(row=2, column=0, padx=10, pady=10)
+
+M_amount = Label(grid_frame, text="0")
+M_amount.grid(row=3, column=0, padx=5, pady=5)
+
+E_label = Label(grid_frame, text="E Credits", font=('Arial', 9, 'underline'))
+E_label.grid(row=2, column=1, padx=10, pady=10)
+
+E_amount = Label(grid_frame, text="0")
+E_amount.grid(row=3, column=1, padx=5, pady=5)
+
+#User entry for Labels above
+grid_frame = Frame(main_menu)
+grid_frame.grid(row=4, column=1, columnspan=2, padx=20, pady=20)
+
+NA_label2 = Label(grid_frame, text="Enter NA Credits")
+NA_label2.grid(row=4, column=0, padx=5, pady=5)
+
+NA_entry = Entry(grid_frame)
+NA_entry.grid(row=5, column=0, padx=5, pady=5)
+
+A_label2 = Label(grid_frame, text="Enter A Credits")
+A_label2.grid(row=4, column=1, padx=5, pady=5)
+
+A_entry = Entry(grid_frame)
+A_entry.grid(row=5, column=1, padx=5, pady=5)
+
+M_label2 = Label(grid_frame, text="Enter M Credits")
+M_label2.grid(row=6, column=0, padx=5, pady=5)
+
+M_entry = Entry(grid_frame)
+M_entry.grid(row=7, column=0, padx=5, pady=5)
+
+E_label2 = Label(grid_frame, text="Enter E Credits")
+E_label2.grid(row=6, column=1, padx=5, pady=5)
+
+E_entry = Entry(grid_frame)
+E_entry.grid(row=7, column=1, padx=5, pady=5)
+
 
 #Logout button
 back_button = Button(main_menu, text="Logout", borderwidth=2, relief="solid", font=style, command=lambda: show_frame(login_menu))
-back_button.grid(row=3, column=2, pady=20, sticky="n")
+back_button.grid(row=8, column=2, pady=20, sticky="n")
 
 #show login menu first
 show_frame(login_menu)
